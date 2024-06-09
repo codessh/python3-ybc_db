5e5156f992853d0e2fe2c38e9545f2adf3392f96
codessh <124234780+codessh@users.noreply.github.com>
2024-06-09 10:02:33 +0800

    ybc_db功能示例

---
+++ functions.py
@@ -0,0 +1,13 @@
+import ybc_db
+
+# Create new Database 新建数据库
+db = ybc_db.create_db('my_database')
+# Create new Collection 创建新集合
+coll = db.collection('my_collection')
+
+# Create new data(dictionary) 创建新数据（字典形式）
+coll.insert_one({'age':11,'name':'codessh'})
+# Find all data(return a list) 输出所有数据（列表形式）
+alld = coll.find_all()
+# Delete a data(search) 删除一个数据（需要查询条件）
+coll.delete_one({'age':10,'name':'MCSRC(Anti-Mini)'})# id:'114514'

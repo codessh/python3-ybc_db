3e86da2a9bd318290b249b7da39cf2b038c9e473
codessh <124234780+codessh@users.noreply.github.com>
2024-06-09 17:25:09 +0800

    ybc_db模块功能示例

--- functions.py
+++ functions.py
@@ -9,5 +9,7 @@ coll = db.collection('my_collection')
 coll.insert_one({'age':11,'name':'codessh'})
 # Find all data(return a list) 输出所有数据（列表形式）
 alld = coll.find_all()
-# Delete a data(search) 删除一个数据（需要查询条件）
+# Find one data 搜索一条数据
+oned = coll.find_one({'name':'MCSRC(Anti-Mini')})
+# Delete one data(search) 删除一个数据（需要查询条件）
 coll.delete_one({'age':10,'name':'MCSRC(Anti-Mini)'})# id:'114514'
